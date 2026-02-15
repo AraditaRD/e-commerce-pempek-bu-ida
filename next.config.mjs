062a123e2d60128e2e3aec06e3982455e2162e92
Aradita Ratu Disanda <araditaratudisanda@gmail.com>
2026-02-15 23:02:21 +0700

    Update Next.js configuration for e-commerce project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/e-commerce-pempek-bu-ida',
+  assetPrefix: '/e-commerce-pempek-bu-ida/',
 };
 
 export default nextConfig;
